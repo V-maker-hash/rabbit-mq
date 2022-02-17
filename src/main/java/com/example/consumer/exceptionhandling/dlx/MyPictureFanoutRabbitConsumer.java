3622bba94b29021f2a7277fe84d814506e6c1c53
V-maker-hash <leonidasway11051989@gmail.com>
2022-02-17 15:00:07 +0300

    Handling dead letter exchange by channel and @Header (№2)

--- src/main/java/com/example/consumer/exceptionhandling/dlx/MyPictureFanoutRabbitConsumer.java
+++ src/main/java/com/example/consumer/exceptionhandling/dlx/MyPictureFanoutRabbitConsumer.java
@@ -1,14 +1,17 @@
 package com.example.consumer.exceptionhandling.dlx;
 
 import com.example.entity.Picture;
-import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
+import com.rabbitmq.client.Channel;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.amqp.AmqpRejectAndDontRequeueException;
 import org.springframework.amqp.rabbit.annotation.RabbitListener;
+import org.springframework.amqp.support.AmqpHeaders;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.messaging.handler.annotation.Header;
 import org.springframework.stereotype.Service;
+
+import java.io.IOException;
 
 @Service
 public class MyPictureFanoutRabbitConsumer {
@@ -19,11 +22,15 @@ public class MyPictureFanoutRabbitConsumer {
     private ObjectMapper objectMapper;
 
     @RabbitListener(queues = {"q.mypicture.image"})
-    public void getMessageFromPictureProducer(String msg) throws JsonProcessingException {
+    public void getMessageFromPictureProducer(String msg, Channel channel,
+                                              @Header(AmqpHeaders.DELIVERY_MODE) long tag) throws IOException {
         var picture = objectMapper.readValue(msg, Picture.class);
         if (picture.getSize() > 9000) {
-            throw new AmqpRejectAndDontRequeueException("Picture size more than 9000");
+//            throw new AmqpRejectAndDontRequeueException("Picture size more than 9000");
+            channel.basicReject(tag, false);
+            return;
         }
         LOGGER.info("Consuming {}", msg);
+        channel.basicAck(tag, false);
     }
 }
