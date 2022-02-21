3552b0a296bcd57881ac04c7e8bece78c5fffd9e
V-maker-hash <leonidasway11051989@gmail.com>
2022-02-21 11:13:53 +0300

    Spring Retry Mechanism for Fanout Exchange

--- src/main/java/com/example/RabbitmqProducerApplication.java
+++ src/main/java/com/example/RabbitmqProducerApplication.java
@@ -1,23 +1,21 @@
 package com.example;
 
-import com.example.entity.Picture;
-import com.example.entity.Source;
-import com.example.entity.Type;
-import com.example.producer.springmechanism.PictureSpringRetryDirectProducer;
+import com.example.entity.Employee;
+import com.example.producer.springmechanism.EmployeeSpringRetryFanoutProducer;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
-import java.util.concurrent.ThreadLocalRandom;
+import java.time.LocalDate;
 
 @SpringBootApplication
 @EnableScheduling
 public class RabbitmqProducerApplication implements CommandLineRunner {
 
     @Autowired
-    private PictureSpringRetryDirectProducer pictureSpringRetryDirectProducer;
+    private EmployeeSpringRetryFanoutProducer employeeSpringRetryFanoutProducer;
 
     public static void main(String[] args) {
         SpringApplication.run(RabbitmqProducerApplication.class, args);
@@ -25,12 +23,11 @@ public class RabbitmqProducerApplication implements CommandLineRunner {
 
     @Override
     public void run(String... args) throws Exception {
-        for (int i = 0; i < 5; i++) {
-            var picture = new Picture("" + i,
-                    Type.getRandomType(),
-                    Source.getRandomSource(),
-                    ThreadLocalRandom.current().nextLong(9000, 10000));
-            pictureSpringRetryDirectProducer.sendMessageFromPictureProducer(picture);
+        for (int i = 0; i < 1; i++) {
+            var picture = new Employee("" + i,
+                    "",
+                    LocalDate.now());
+            employeeSpringRetryFanoutProducer.sendEmployeeInfo(picture);
         }
     }
 }
--- src/main/java/com/example/producer/springmechanism/EmployeeSpringRetryFanoutProducer.java
+++ src/main/java/com/example/producer/springmechanism/EmployeeSpringRetryFanoutProducer.java
@@ -1,14 +1,14 @@
 package com.example.producer.springmechanism;
 
-import com.example.entity.Picture;
+import com.example.entity.Employee;
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import org.springframework.amqp.rabbit.core.RabbitTemplate;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
-//@Service
-public class PictureSpringRetryDirectProducer {
+@Service
+public class EmployeeSpringRetryFanoutProducer {
 
     @Autowired
     private ObjectMapper objectMapper;
@@ -16,8 +16,8 @@ public class PictureSpringRetryDirectProducer {
     @Autowired
     private RabbitTemplate rabbitTemplate;
 
-    public void sendMessageFromPictureProducer(Picture picture) throws JsonProcessingException {
-        String value = objectMapper.writeValueAsString(picture);
-        rabbitTemplate.convertAndSend("x.spring.work", picture.getType(), value);
+    public void sendEmployeeInfo(Employee employee) throws JsonProcessingException {
+        var value = objectMapper.writeValueAsString(employee);
+        rabbitTemplate.convertAndSend("x.spring2.work", "", value);
     }
 }
